6dc9abc262cdd1d572feba7fd68f2a490b77145e
Marifei <116741086+Marina-Bordug@users.noreply.github.com>
2023-05-10 17:14:59 +0300

    final sort

--- static/scripts/sort-doc.js
+++ static/scripts/sort-doc.js
@@ -6,76 +6,86 @@ let headers = document.querySelectorAll(".marks-2 th");
 
 
 headers.forEach(function(header){
-    console.log(1)
     header.addEventListener("click", function(e){
         let sortedRows = Array.from(rows)
         if (e.target.textContent == 'Уровень') {
-            sortedRows.sort(function(a, b){
-                return a.cells[0].innerHTML > b.cells[0].innerHTML ? 1 : -1;
-            });
+            if (e.target.dataset.sort == 'up') {
+                sortedRows.sort(function(a, b){
+                    return a.cells[0].innerHTML > b.cells[0].innerHTML ? 1 : -1;
+                });
+                e.target.dataset.sort = 'down'
+            } else {
+                sortedRows.sort(function(a, b){
+                    return a.cells[0].innerHTML < b.cells[0].innerHTML ? 1 : -1;
+                });
+                e.target.dataset.sort = 'up'
+            }
+
+           
             tbody.append(...sortedRows);
         }
+        
         if (e.target.textContent == 'Предмет') {
+            if (e.target.dataset.sort == 'up') {
             sortedRows.sort(function(a, b){
                 return a.cells[1].innerHTML > b.cells[1].innerHTML ? 1 : -1;
             });
+            e.target.dataset.sort = "down"
+        } else {
+            sortedRows.sort(function(a, b){
+                return a.cells[1].innerHTML < b.cells[1].innerHTML ? 1 : -1;
+            });
+            e.target.dataset.sort = 'up'
+        }
+
             tbody.append(...sortedRows);
         }
         if (e.target.textContent == 'Название') {
-            sortedRows.sort(function(a, b){
-                return a.cells[2].innerHTML > b.cells[2].innerHTML ? 1 : -1;
-            });
+            if (e.target.dataset.sort == 'up') {
+                sortedRows.sort(function(a, b){
+                    return a.cells[2].innerHTML > b.cells[2].innerHTML ? 1 : -1;
+                });
+                e.target.dataset.sort = "down"
+            } else {
+                sortedRows.sort(function(a, b){
+                    return a.cells[2].innerHTML < b.cells[2].innerHTML ? 1 : -1;
+                });
+                e.target.dataset.sort = 'up'
+            }
+            
             tbody.append(...sortedRows);
         }
         if (e.target.textContent == 'Дата') {
-            sortedRows.sort(function(a, b){
-                return new Date(a.cells[3].innerHTML) > new Date(b.cells[3].innerHTML) ? 1 : -1;
-            });
+            if (e.target.dataset.sort == 'up') {
+                sortedRows.sort(function(a, b){
+                    return new Date(a.cells[3].innerHTML) > new Date(b.cells[3].innerHTML) ? 1 : -1;
+                }); 
+                e.target.dataset.sort = "down"
+            } else {
+                sortedRows.sort(function(a, b){
+                    return a.cells[3].innerHTML < b.cells[3].innerHTML ? 1 : -1;
+                }); 
+                e.target.dataset.sort = 'up'
+            } 
+            
             tbody.append(...sortedRows);
         }
         if (e.target.textContent == 'Результаты') {
-            sortedRows.sort(function(a, b){
-                return a.cells[4].innerHTML > b.cells[4].innerHTML ? 1 : -1;
-            });
+            if (e.target.dataset.sort == 'up') {
+                sortedRows.sort(function(a, b){
+                    return a.cells[4].innerHTML > b.cells[4].innerHTML ? 1 : -1;
+                });
+                e.target.dataset.sort = "down"
+            } else {
+                sortedRows.sort(function(a, b){
+                    return a.cells[4].innerHTML < b.cells[4].innerHTML ? 1 : -1;
+                });
+                e.target.dataset.sort = 'up'
+            }
+            
             tbody.append(...sortedRows);
         }
     });
 });
 
-headers.forEach(function(header){
-    header.addEventListener("dblclick", function(e){
-        let reverseRows = Array.from(rows)
-        if (e.target.textContent == 'Уровень') {
-            reverseRows.reverse(function(a, b){
-                return a.cells[0].innerHTML < b.cells[0].innerHTML ? 1 : -1;
-            });
-            tbody.append(...reverseRows);
-        }
-        
-        if (e.target.textContent == 'Предмет') {
-            reverseRows.reverse(function(a, b){
-                return a.cells[1].innerHTML < b.cells[1].innerHTML ? 1 : -1;
-            });
-            tbody.append(...reverseRows);
-        }
-        if (e.target.textContent == 'Название') {
-            reverseRows.reverse(function(a, b){
-                return a.cells[2].innerHTML < b.cells[2].innerHTML ? 1 : -1;
-            });
-            tbody.append(...reverseRows);
-        }
-        if (e.target.textContent == 'Дата') {
-            reverseRows.reverse(function(a, b){
-                return new Date(a.cells[3].innerHTML) < new Date(b.cells[3].innerHTML) ? 1 : -1;
-            });
-            tbody.append(...reverseRows);
-        }
-        if (e.target.textContent == 'Результаты') {
-            reverseRows.reverse(function(a, b){
-                return a.cells[4].innerHTML < b.cells[4].innerHTML ? 1 : -1;
-            });
-            tbody.append(...reverseRows);
-        }
-    });
-});
 
